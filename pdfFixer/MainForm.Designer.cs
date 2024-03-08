f9ceacbacad85fb4ea3dfcaa869a99c5ab33ab23
Emory.Kempf <emo.k@msseggs.com>
2024-03-08 07:39:41 -0600

    Add project files.

---
+++ pdfFixer/MainForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace pdfFixer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.flp1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnSaveAsNewPDF = new DevExpress.XtraEditors.SimpleButton();
+            this.txtOutputFolderPath = new DevExpress.XtraEditors.TextEdit();
+            this.label1 = new DevExpress.XtraEditors.LabelControl();
+            this.label2 = new DevExpress.XtraEditors.LabelControl();
+            this.txtFileName = new DevExpress.XtraEditors.TextEdit();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnChooseFolder = new DevExpress.XtraEditors.SimpleButton();
+            this.cbDeleteOriginals = new DevExpress.XtraEditors.CheckEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtOutputFolderPath.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFileName.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cbDeleteOriginals.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flp1
+            // 
+            this.flp1.AllowDrop = true;
+            this.flp1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flp1.AutoScroll = true;
+            this.flp1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flp1.Location = new System.Drawing.Point(12, 47);
+            this.flp1.Name = "flp1";
+            this.flp1.Size = new System.Drawing.Size(1440, 820);
+            this.flp1.TabIndex = 0;
+            this.flp1.DragDrop += new System.Windows.Forms.DragEventHandler(this.flp1_DragDrop);
+            this.flp1.DragEnter += new System.Windows.Forms.DragEventHandler(this.flp1_DragEnter);
+            // 
+            // btnSaveAsNewPDF
+            // 
+            this.btnSaveAsNewPDF.Appearance.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F);
+            this.btnSaveAsNewPDF.Appearance.Options.UseFont = true;
+            this.btnSaveAsNewPDF.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnSaveAsNewPDF.ImageOptions.Image")));
+            this.btnSaveAsNewPDF.ImageOptions.Location = DevExpress.XtraEditors.ImageLocation.MiddleRight;
+            this.btnSaveAsNewPDF.Location = new System.Drawing.Point(12, 5);
+            this.btnSaveAsNewPDF.Name = "btnSaveAsNewPDF";
+            this.btnSaveAsNewPDF.Size = new System.Drawing.Size(147, 36);
+            this.btnSaveAsNewPDF.TabIndex = 1;
+            this.btnSaveAsNewPDF.Text = "Save As New PDF";
+            this.btnSaveAsNewPDF.Click += new System.EventHandler(this.btnSaveAsNewPDF_Click);
+            // 
+            // txtOutputFolderPath
+            // 
+            this.txtOutputFolderPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutputFolderPath.Location = new System.Drawing.Point(558, 9);
+            this.txtOutputFolderPath.Name = "txtOutputFolderPath";
+            this.txtOutputFolderPath.Properties.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutputFolderPath.Properties.Appearance.Options.UseFont = true;
+            this.txtOutputFolderPath.Size = new System.Drawing.Size(565, 34);
+            this.txtOutputFolderPath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Appearance.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Appearance.Options.UseFont = true;
+            this.label1.Location = new System.Drawing.Point(404, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Output folder path";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Appearance.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Appearance.Options.UseFont = true;
+            this.label2.Location = new System.Drawing.Point(1185, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File Name";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFileName.Location = new System.Drawing.Point(1269, 9);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Properties.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFileName.Properties.Appearance.Options.UseFont = true;
+            this.txtFileName.Size = new System.Drawing.Size(183, 34);
+            this.txtFileName.TabIndex = 4;
+            // 
+            // btnChooseFolder
+            // 
+            this.btnChooseFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChooseFolder.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnChooseFolder.ImageOptions.SvgImage")));
+            this.btnChooseFolder.ImageOptions.SvgImageSize = new System.Drawing.Size(30, 30);
+            this.btnChooseFolder.Location = new System.Drawing.Point(1081, 9);
+            this.btnChooseFolder.Name = "btnChooseFolder";
+            this.btnChooseFolder.Size = new System.Drawing.Size(42, 33);
+            this.btnChooseFolder.TabIndex = 6;
+            this.btnChooseFolder.Click += new System.EventHandler(this.btnChooseFolder_Click);
+            // 
+            // cbDeleteOriginals
+            // 
+            this.cbDeleteOriginals.Location = new System.Drawing.Point(165, 14);
+            this.cbDeleteOriginals.Name = "cbDeleteOriginals";
+            this.cbDeleteOriginals.Properties.Appearance.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.cbDeleteOriginals.Properties.Appearance.Options.UseFont = true;
+            this.cbDeleteOriginals.Properties.Caption = "Delete original files";
+            this.cbDeleteOriginals.Properties.ImageOptions.SvgImageGrayed = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("cbDeleteOriginals.Properties.ImageOptions.SvgImageGrayed")));
+            this.cbDeleteOriginals.Size = new System.Drawing.Size(182, 22);
+            this.cbDeleteOriginals.TabIndex = 7;
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1468, 879);
+            this.Controls.Add(this.cbDeleteOriginals);
+            this.Controls.Add(this.btnChooseFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutputFolderPath);
+            this.Controls.Add(this.btnSaveAsNewPDF);
+            this.Controls.Add(this.flp1);
+            this.IconOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("MainForm.IconOptions.SvgImage")));
+            this.Name = "MainForm";
+            this.Text = "PdfFixer";
+            ((System.ComponentModel.ISupportInitialize)(this.txtOutputFolderPath.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFileName.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cbDeleteOriginals.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flp1;
+        private DevExpress.XtraEditors.SimpleButton btnSaveAsNewPDF;
+        private DevExpress.XtraEditors.TextEdit txtOutputFolderPath;
+        private DevExpress.XtraEditors.LabelControl label1;
+        private DevExpress.XtraEditors.LabelControl label2;
+        private DevExpress.XtraEditors.TextEdit txtFileName;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private DevExpress.XtraEditors.SimpleButton btnChooseFolder;
+        private DevExpress.XtraEditors.CheckEdit cbDeleteOriginals;
+    }
+}
+
---
+++ pdfFixer/MainForm.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraPdfViewer.Forms;
+using DevExpress.XtraPdfViewer;
+using PdfSharp;
+using PdfSharp.Pdf;
+using PdfSharp.Forms;
+using System.IO;
+using PdfSharp.Pdf.IO;
+using System.Runtime.CompilerServices;
+using DevExpress.XtraBars.Controls;
+using DevExpress.Emf;
+using DevExpress.XtraEditors;
+using pdfFixer.Properties;
+using System.Resources;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace pdfFixer
+{
+    public partial class MainForm : XtraForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            FormClosing += MainForm_FormClosing;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            while(flp1.Controls.Count > 0)
+            {
+                Panel panel = (Panel)flp1.Controls[0];
+                panel.Dispose();
+            }
+
+            foreach(var file in NewFiles)
+            {
+                File.Delete(file);
+            }
+        }
+
+        List<string> OriginalFiles { get; set; } = new List<string>();
+        List<string> NewFiles { get; set; } = new List<string>();
+        private void flp1_DragDrop(object sender, DragEventArgs e)
+        {
+            var data = e.Data;
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            var pdfviewer = e.Data.GetDataPresent(typeof(Panel));
+
+            if (files != null)
+                foreach (string file in files)
+                {
+                    if (!file.ToLower().EndsWith(".pdf"))
+                        continue;
+
+                    OriginalFiles.Add(file);
+
+                    AddPDF(file.ToString(), sender as FlowLayoutPanel);
+                }
+
+            AddPageNumbers();
+        }
+        private void AddPDF(string filepath, FlowLayoutPanel pnl)
+        {
+            PdfDocument pdf = new PdfDocument();
+            pdf = PdfReader.Open(filepath, PdfDocumentOpenMode.Import);
+            var pages = pdf.Pages;
+            var pagnum = 1;
+            foreach (var page in pages)
+            {
+                PdfDocument pagePdf = new PdfDocument();
+                pagePdf.AddPage(page);
+                var dir = Path.GetDirectoryName(filepath);
+                var file = Guid.NewGuid().ToString() + ".pdf";
+                var newPath = Path.Combine(dir, file);
+                pagePdf.Save(newPath);
+
+                NewFiles.Add(newPath);
+
+                MyPDFviewer myPDFviewer = new MyPDFviewer();
+                myPanel panel = new myPanel();
+                panel.FilePath = newPath;
+                panel.Height = 520;
+                panel.Width = 350;
+                panel.Margin = new Padding(1);
+                panel.SetPage(pagnum);
+                myPDFviewer.LoadDocument(newPath);
+                myPDFviewer.Top = panel.Top + 15;
+                myPDFviewer.Width = 370;
+                myPDFviewer.Height = 500;
+                myPDFviewer.ZoomMode = PdfZoomMode.FitToVisible;
+                myPDFviewer.AllowDrop = true;
+                myPDFviewer.MouseDown += MyPDFviewer_MouseDown;
+                myPDFviewer.DragEnter += MyPDFviewer_DragEnter;
+                myPDFviewer.DragDrop += MyPDFviewer_DragDrop;
+                myPDFviewer.Name = "PdfViewer_" + pagnum.ToString();
+
+                panel.Name = "Panel_" + pagnum.ToString();
+                panel.MouseEnter += Panel_MouseEnter;
+                panel.MouseLeave += Panel_MouseLeave;
+                panel.AllowDrop = true;
+                panel.Controls.Add(myPDFviewer);
+                panel.MouseDown += Panel_MouseDown;
+                panel.pDFviewer = myPDFviewer;
+                pnl.Controls.Add(panel);
+                txtOutputFolderPath.Text = dir;
+
+                var btn = new SimpleButton(); 
+                btn.Location = new Point(150, 476);
+                btn.Size = new System.Drawing.Size(80, 20);
+                btn.Click += Btn_Click;
+                panel.Controls.Add(btn);
+                btn.Name = "btn";
+                btn.Text = "Remove page";
+                
+                btn.BringToFront();
+                pagnum++;
+            }
+
+            //Pdfd
+
+
+        }
+        private void RemoveNewFile(string filepath)
+        {
+            NewFiles.Remove(filepath);
+            File.Delete(filepath);
+        }
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            myPanel pnl = (myPanel)((SimpleButton)sender).Parent;
+            flp1.Controls.Remove(pnl);
+            pnl.Dispose();
+            RemoveNewFile(pnl.FilePath);
+
+            AddPageNumbers();
+        }
+
+        private myPanel LeftPanel { get; set; } = new myPanel();
+        private void Panel_MouseLeave(object sender, EventArgs e)
+        {
+            
+            LeftPanel = sender as myPanel;
+        }
+
+        private Panel CurrentPanel { get; set; }
+
+        private void Panel_MouseEnter(object sender, EventArgs e)
+        {
+            var target = sender as Panel;
+            CurrentPanel = target;
+
+        }
+        public class DragDropInfo
+        {
+            public myPanel control { get; set; }
+            public DragDropInfo(myPanel ctl)
+            {
+                this.control = ctl;
+            }
+        }
+
+        private void MyPDFviewer_DragDrop(object sender, DragEventArgs e)
+        {
+            var target = FindControlAtCursor(this);
+            var index = flp1.Controls.GetChildIndex(target);
+
+            DragDropInfo fromdrop = (DragDropInfo)e.Data.GetData(typeof(DragDropInfo));
+            var src = fromdrop.control as myPanel;
+
+            if (LeftPanel.Name == string.Empty)
+                return;
+
+            var leftIndex = flp1.Controls.GetChildIndex(LeftPanel);
+
+            if (index > -1)
+                flp1.Controls.SetChildIndex(src, index);
+
+            AddPageNumbers();
+        }
+        private void MyPDFviewer_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void MyPDFviewer_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+
+            base.OnMouseDown(e);
+            var panel = (myPanel)(sender as Control).Parent as myPanel;
+            DoDragDrop(new DragDropInfo(panel), DragDropEffects.All);
+        }
+
+        private void Panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+
+            base.OnMouseDown(e);
+            DoDragDrop(sender, DragDropEffects.All);
+        }
+
+        private void flp1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void flp2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+        public class MyPDFviewer : PdfViewer
+        {
+            public MyPDFviewer()
+            {
+                NavigationPaneInitialVisibility = PdfNavigationPaneVisibility.Hidden;
+            }
+        }
+        public class myPanel : Panel
+        {
+            public string FilePath { get; set; }
+            public int PageNumber { get; set; }
+            public Label pageLabel { get; set; } = new Label();
+            public MyPDFviewer pDFviewer { get; set; }
+            public void SetPage(int page)
+            {
+                PageNumber = page;
+
+            }
+        }
+        public static Control FindControlAtPoint(Control container, Point pos)
+        {
+            Control child;
+            foreach (Control c in container.Controls)
+            {
+                if (!(c is Panel))
+                    continue;
+                if (c.Visible && c.Bounds.Contains(pos))
+                {
+                    child = FindControlAtPoint(c, new Point(pos.X - c.Left, pos.Y - c.Top));
+                    if (child == null) return c;
+                    else return child;
+                }
+            }
+            return null;
+        }
+        public static Control FindControlAtCursor(Form form)
+        {
+            Point pos = Cursor.Position;
+            if (form.Bounds.Contains(pos))
+                return FindControlAtPoint(form, form.PointToClient(pos));
+            return null;
+        }
+
+        private void btnSaveAsNewPDF_Click(object sender, EventArgs e)
+        {
+            SaveAsNew();
+        }
+        private void SaveAsNew()
+        {
+            if (txtFileName.Text.Trim().Length < 1)
+            {
+                MessageBox.Show("Choose a new filename first!");
+                return;
+            }
+
+            PdfDocument outputDoc = new PdfDocument();
+
+            while (flp1.Controls.Count > 0)
+            {
+                var panel = (Panel)flp1.Controls[0];
+                myPanel mp = (myPanel)panel;
+                MyPDFviewer myPDFviewer = new MyPDFviewer();
+                foreach (Control c in mp.Controls)
+                    if (c is MyPDFviewer)
+                        myPDFviewer = (MyPDFviewer)c;
+
+                var fp = mp.FilePath;
+                PdfDocument input = PdfReader.Open(fp, PdfDocumentOpenMode.Import);
+                PdfPage page = input.Pages[0];
+
+                page.Rotate = myPDFviewer.RotationAngle;
+                outputDoc.AddPage(page);
+
+                RemoveNewFile(fp);
+
+                panel.Dispose();
+                File.Delete(fp);
+            }
+            var dir = txtOutputFolderPath.Text + "\\";
+            var filen = txtFileName.Text + ".pdf";
+            outputDoc.Save(dir + filen);
+
+            if (cbDeleteOriginals.Checked)
+                DeleteOriginals();
+        }
+        private void DeleteOriginals()
+        {
+            foreach (var file in OriginalFiles)
+            {
+                if (File.Exists(file))
+                    File.Delete(file);
+            }
+            OriginalFiles.Clear();
+        }
+        private void AddPageNumbers()
+        {
+            foreach (Control ctrl in flp1.Controls)
+                if (ctrl is Panel)
+                    foreach (Control lbl in ctrl.Controls)
+                        if (lbl is LabelControl)
+                            flp1.Controls.Remove(lbl);
+
+            var pg = 1;
+            foreach (Control c in flp1.Controls)
+            {
+                if (!(c is Panel))
+                    return;
+                var mp = (myPanel)c;
+                var pageLabel = new LabelControl();
+                pageLabel.Text = "Page " + pg.ToString();
+                pageLabel.Visible = true;
+                pageLabel.BackColor = Color.Black;
+                pageLabel.Location = new System.Drawing.Point(150, 20);
+                pageLabel.Width = 500;
+                
+                mp.Controls.Add(pageLabel);
+                pageLabel.BringToFront();
+                pg++;
+            }
+        }
+
+        private void btnChooseFolder_Click(object sender, EventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+
+            if(dialog.ShowDialog() != CommonFileDialogResult.Ok)
+                return;
+
+            txtOutputFolderPath.Text = dialog.FileName;
+        }
+    }
+}
